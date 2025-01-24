a39190027f45c86202e619fbdb19888e70311a96
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2025-01-24 20:11:05 -0300

    [#3] Refactor login component layout and implement form handling with validation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
+  constructor() {
+    this.email = '';
+    this.password = '';
+  }
+
+  ngOnInit(): void {}
+
+  login(event: any): void {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    console.log('Event:', event);
+  }
 }
